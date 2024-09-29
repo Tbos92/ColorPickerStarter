2f25e8fe7a982176aa0d69cfd46290e733070b8f
Tyler <tbos92@gmail.com>
2024-09-29 15:44:37 -0400

    Color picker commit with optional selected outline

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 🎨 Color Picker
 
 Follow the instructions in the workshop to finish the assignment!
+
+deployed build link: https://66f9adbbf362fc52ab9724b8--superlative-centaur-6442c0.netlify.app/
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
+
 // Write your Color component here
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  const isSelected = color === selectedColor ? "selected" : "";
+  return (
+    <div
+      className={`${color} ${isSelected}`}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="red"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="yellow"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="violet"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+      </div>
     </div>
   );
 };
